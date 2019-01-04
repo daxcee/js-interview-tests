942f963db06f77524591e75147083198f8a0e25c
Nick Guernsey <n.guerns@gmail.com>
2019-01-04 08:25:44 -0500

    Updated checkPrime

--- check-prime/checkPrime.js
+++ check-prime/checkPrime.js
@@ -6,14 +6,15 @@
  */
 function isPrime(n) {
   if (typeof n !== 'number') throw new Error('Must be a valid number');
-  if (n === 2) return true;
+  if (n === 2 || n === 3) return true;
   if (n < 2) return false;
 
   let divisor = 2;
+  const limit = Math.sqrt(n);
 
-  while ( n > divisor) {
+  while ( limit >= divisor) {
     if (n % divisor === 0) return false;
-    divisor += 1;
+    divisor += 2;
   }
 
   return true;
